8e62a8c7c9c93c0a6e3b9fed423efbdde6eab8ba
alexkorsky <koral7@yahoo.com>
2017-05-20 16:38:48 -0500

    My changes for project

--- Udacity120/validation/validate_poi.py
+++ Udacity120/validation/validate_poi.py
@@ -15,18 +15,35 @@ import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 
-data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
+data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "rb") )
 
 ### first element is our labels, any added elements are predictor
 ### features. Keep this the same for the mini-project, but you'll
 ### have a different feature list when you do the final project.
 features_list = ["poi", "salary"]
 
-data = featureFormat(data_dict, features_list)
+data = featureFormat(data_dict, features_list, sort_keys = '../tools/python2_lesson13_keys.pkl')
 labels, features = targetFeatureSplit(data)
 
+from sklearn import cross_validation
+features_Train, features_Test, labels_Train, labels_Test = cross_validation.train_test_split(features, labels,
+                                                                                            test_size = 0.3, 
+                                                                                            random_state = 42)
+from sklearn.tree import DecisionTreeClassifier
+clf = DecisionTreeClassifier()
+clf.fit(features_Train, labels_Train)
+
+pred=clf.predict(features_Test)
 
 
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_Test)
+
+
+score = clf.score(features_Test, labels_Test)
+
+print(score)
+print(acc)
 ### it's all yours from here forward!  
 
 
