3c8f22b5832017521dd9ad6d70dd74ee24e60844
alexkorsky <koral7@yahoo.com>
2017-08-14 07:51:46 -0500

    First PlotLib

--- Udacity120/final_project/poi_id.py
+++ Udacity120/final_project/poi_id.py
@@ -2,6 +2,7 @@
 
 import sys
 import pickle
+import numpy as np
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
@@ -13,8 +14,71 @@ from tester import dump_classifier_and_data
 features_list = ['poi','salary'] # You will need to use more features
 
 ### Load the dictionary containing the dataset
-with open("final_project_dataset.pkl", "r") as data_file:
+with open("final_project_dataset.pkl", "rb") as data_file:
     data_dict = pickle.load(data_file)
+
+
+researchfeatures = ( 
+'bonus',
+'deferral_payments',
+'deferred_income',
+'director_fees',
+'email_address',
+'exercised_stock_options',
+'expenses',
+'from_messages',
+'from_poi_to_this_person',
+'from_this_person_to_poi',
+'loan_advances',
+'long_term_incentive',
+'other',
+'poi',
+'restricted_stock',
+'restricted_stock_deferred',
+'salary',
+'shared_receipt_with_poi',
+'to_messages',
+'total_payments',
+'total_stock_value'
+)
+
+# create map of ids to Person name
+id = 0
+id_name_dictionary = {}
+name_id_dictionary = {}
+for key in data_dict:
+    if (key != "TOTAL"):
+        id_name_dictionary[id] = key
+        name_id_dictionary[key] = id
+        id = id + 1
+    
+import matplotlib.pyplot as plt
+for feature in researchfeatures:
+    print (feature);
+    if (feature != 'email_address' and feature == 'bonus'):
+        points = []
+        colors = []
+        labels = []
+        for key in data_dict:
+            if (key != "TOTAL" and data_dict[key][feature] != 'NaN'):
+                points.append(float(data_dict[key][feature]) / 10000)
+                labels.append(name_id_dictionary[key])
+                if (data_dict[key]['poi'] == 1 ):
+                    colors += 'r'
+                else:
+                    colors += 'b'
+            #color += data_dict[key]['poi'] == 1 ? 'r' : 'b'                
+      
+        plt.xlim(0, int(max(labels)))
+        plt.ylim(0, int(max(points)))
+        plt.axis([0, int(max(labels)), 0 , int(max(points))]) 
+
+        plt.scatter(labels, points, color=colors )
+
+        plt.show()
+
+    
+'''
 
 ### Task 2: Remove outliers
 ### Task 3: Create new feature(s)
@@ -53,3 +117,5 @@ features_train, features_test, labels_train, labels_test = \
 ### generates the necessary .pkl files for validating your results.
 
 dump_classifier_and_data(clf, my_dataset, features_list)
+
+'''
